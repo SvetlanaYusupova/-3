21b8a700d86dc3671484ece4fe9480c3570cb5c1
SvetlanaYusupova <96313848+SvetlanaYusupova@users.noreply.github.com>
2021-12-18 01:58:01 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,19 +9,19 @@ class TestDataProcessing(unittest.TestCase):
         data = [1, 2, 3, 4.1]
         assert isclose(1, minimal(data))
 
-    if True:    # Тест проверки функции нахождения максимума
+    def test_max(self):    # Тест проверки функции нахождения максимума
         data = [4.2, 4, 4.2, 4.1]
         assert isclose(4.2, maximum(data))
 
-    if True:    # Тест проверки функции нахождения суммы
+    def test_sum(self):    # Тест проверки функции нахождения суммы
         data = [-2.1, 2.1, -4.1, 4.1]
         assert isclose(0, summ(data))
 
-    if True:    # Тест проверки функции нахождения произведения
+    def test_pro(self):    # Тест проверки функции нахождения произведения
         data = [-2, 21, -4.1, 8]
         assert isclose(1377.6, product(data))
 
-    if True:    # Тест проверки файла на скорость при увеличении количества данных в файле в 5000 раз
+    def test_speed(self):    # Тест проверки файла на скорость при увеличении количества данных в файле в 5000 раз
         try:
             my_file = open("test_1.txt", "w+")
             my_file.write(str(sample(range(-100000, 100000), 10)))
@@ -49,7 +49,7 @@ class TestDataProcessing(unittest.TestCase):
         except AssertionError:
             print("Test on speed losed")
 
-    if True:    # Тест проверки всех функций на рандомном количестве рандомных чисел 1000 раз подряд
+    def test_add(self):    # Тест проверки всех функций на рандомном количестве рандомных чисел 1000 раз подряд
         for i in range(1000):
             lengh = int(sample(range(1, 100), 1)[0])
             data = sample(range(-1000, 1000), lengh)
